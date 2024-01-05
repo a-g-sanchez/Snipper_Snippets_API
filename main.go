0b82106a2ae1d9dc9c1df37114ae657649b2b26c
a-g-sanchez <aarongsanchez@google.com>
2024-01-05 11:49:23 -0600

    Can read the yaml file and create a slice of snippets

--- main.go
+++ main.go
@@ -1,17 +1,15 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
+import "github.com/a-g-sanchez/Snipper_Snippets_API/util"
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run()
+	// r := gin.Default()
+	// r.GET("/ping", func(c *gin.Context) {
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"message": "pong",
+	// 	})
+	// })
+	// r.Run()
+
+	util.ParseYaml()
 }
--- util/yamlReader.go
+++ util/yamlReader.go
@@ -1,5 +1,31 @@
 package util
 
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Snippet struct {
+	Id       int    `yaml:"id"`
+	Language string `yaml:"language"`
+	Code     string `yaml:"code"`
+}
+
 func ParseYaml() {
 
+	var snippetSlice []Snippet
+
+	data, err := os.ReadFile("seedData.yaml")
+	if err != nil {
+		fmt.Println("Read File:", err)
+	}
+
+	err = yaml.Unmarshal(data, &snippetSlice)
+	if err != nil {
+		fmt.Printf("Unmashal data: %d\n", err)
+	}
+
+	fmt.Println(snippetSlice)
 }
