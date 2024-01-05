29e544fdad6f6eae6034bb20c2a29c958cf8bf75
a-g-sanchez <aarongsanchez@google.com>
2024-01-05 15:01:33 -0600

    Added route to find a snippet by id

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/a-g-sanchez/Snipper_Snippets_API/util"
 	"github.com/gin-gonic/gin"
@@ -14,12 +15,38 @@ func main() {
 	router := gin.Default()
 
 	// ROUTES
+	// GET all snippets
 	router.GET("/snippets", func(c *gin.Context) {
 		c.IndentedJSON(http.StatusOK, gin.H{
 			"message": slice,
 		})
 	})
 
+	// GET a snippet by ID
+	router.GET("/snippets/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		snippetId, err := strconv.Atoi(id)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": err.Error(),
+			})
+		}
+
+		var foundSnippet *util.Snippet
+
+		for _, snippet := range slice {
+			if snippet.Id == snippetId {
+				foundSnippet = &snippet
+				break
+			}
+		}
+
+		c.IndentedJSON(http.StatusOK, gin.H{
+			"message": foundSnippet,
+		})
+	})
+
+	// POST / create a new snippet
 	router.POST("/snippets", func(c *gin.Context) {
 		var newSnippet *util.Snippet
 		if err := c.ShouldBindJSON(&newSnippet); err != nil {
@@ -31,7 +58,7 @@ func main() {
 		slice = append(slice, *newSnippet)
 
 		c.IndentedJSON(http.StatusCreated, gin.H{
-			"message": slice,
+			"message": newSnippet,
 		})
 	})
 
