7900877e80408b439034fcf8f817961b8fffc370
a-g-sanchez <aarongsanchez@google.com>
2024-01-05 15:38:19 -0600

    Implemented the ability to query for a specific language

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/a-g-sanchez/Snipper_Snippets_API/util"
 	"github.com/gin-gonic/gin"
@@ -15,8 +16,25 @@ func main() {
 	router := gin.Default()
 
 	// ROUTES
-	// GET all snippets
+	// GET all snippets or a snippet based on a query for a certain language
 	router.GET("/snippets", func(c *gin.Context) {
+		lang := c.Query("lang")
+
+		if lang != "" {
+			var filteredSnippets []util.Snippet
+
+			for _, snippet := range slice {
+				if strings.EqualFold(lang, snippet.Language) {
+					filteredSnippets = append(filteredSnippets, snippet)
+				}
+			}
+
+			c.IndentedJSON(http.StatusOK, gin.H{
+				"message": filteredSnippets,
+			})
+			return
+		}
+
 		c.IndentedJSON(http.StatusOK, gin.H{
 			"message": slice,
 		})
@@ -58,6 +76,7 @@ func main() {
 		slice = append(slice, *newSnippet)
 
 		c.IndentedJSON(http.StatusCreated, gin.H{
+			"status":  "Created",
 			"message": newSnippet,
 		})
 	})
