67e44275e5687d29677758a323941c8e139c53df
a-g-sanchez <aarongsanchez@google.com>
2024-01-05 10:45:21 -0600

    Initial file structure set up

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/a-g-sanchez/Snipper_Snippets_API
+
+go 1.21.3
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	// "github.com/gin-gonic/gin"
+	// "net/http"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("This is a test")
+}
